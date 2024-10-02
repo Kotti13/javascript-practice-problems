5af2ab1fe16053294ec4957bac378243670e5aea
Kotti13 <skottai493@gmail.com>
2024-10-02 20:57:08 +0530

    problem-solving

---
+++ Problem-solving.js
@@ -0,0 +1,12 @@
+//find between 1 to 1000  sum of all multiples 3 or 5
+// let sum=0;
+// for(let i=1;i<1000;i++){
+//     if(i%3==0||i%5==0){
+//         sum=sum+i;
+//     }
+   
+// }
+//  console.log(sum);
+
+
+ //
